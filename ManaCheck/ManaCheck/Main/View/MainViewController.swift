6dab80111c4bfcda96fe577349aaec23ea1a7e62
eunseo <neruneru33@naver.com>
2025-07-26 11:14:43 +0900

    [Feat] 초기세팅

---
+++ ManaCheck/ManaCheck/Main/View/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManaCheck
+//
+//  Created by 김은서 on 7/26/25.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
---
+++ ManaCheck/ManaCheck/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  MainViewModel.swift
+//  ManaCheck
+//
+//  Created by 김은서 on 7/26/25.
+//
+
